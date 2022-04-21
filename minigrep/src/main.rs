f4cbb4deb532c66207764f9e6da38c2a0a4a3306
linbinghe <linbinghe@gmail.com>
2022-04-21 14:39:35 +0800

    feat: 提交命令行程序完整代码

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,37 +1,17 @@
 use std::env;
-use std::fs;
 use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments:{}", err);
+        eprintln!("Problem parsing arguments:{}", err);
         process::exit(1);
     });
 
-    run(config);
-}
-
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file.");
-
-}
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(Config {query, filename})
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error:{}", e);
+        process::exit(1);
     }
 }
-
