46cc676ccd9de11d3fdfa35b6e6cebeb281b621a
zhangliang <linbinghe@gmail.com>
2022-05-12 09:55:02 +0800

    feat: Android Rust 基础运行 Demo
    需要依赖Python2版本

---
+++ run-rust-on-android/app/src/main/java/com/example/rust/JNICallback.java
@@ -0,0 +1,5 @@
+package com.example.rust;
+
+public interface JNICallback {
+    public void callback(String string);
+}
--- run-rust-on-android/app/src/main/java/com/example/rust/MainActivity.java
+++ run-rust-on-android/app/src/main/java/com/example/rust/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.rust;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements JNICallback {
+
+    static {
+        System.loadLibrary("rust");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        invokeCallbackViaJNI(this);
+    }
+
+    public static native void invokeCallbackViaJNI(JNICallback callback);
+
+    @Override
+    public void callback(String string) {
+        Log.e("Rust", "From JNI: " + string + "\n");
     }
 }
